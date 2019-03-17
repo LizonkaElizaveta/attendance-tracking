e6a6617a58f712b8d8e77e84dad9337a22274f93
Liza <liza_soft_1313@mail.ru>
2019-03-17 23:07:43 +0300

    send data to firebase

--- AttendanceTracking-android/app/src/main/java/stanevich/elizaveta/attendancetracking/MainActivity.java
+++ AttendanceTracking-android/app/src/main/java/stanevich/elizaveta/attendancetracking/MainActivity.java
@@ -1,14 +1,36 @@
 package stanevich.elizaveta.attendancetracking;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    Button registration;
+    private Button registration;
+    private FirebaseAuth mAuth;
+    private DatabaseReference mReference;
+
+    FirebaseUser user = mAuth.getInstance().getCurrentUser();
+
+    FirebaseListAdapter mAdapter;
+
+    ArrayList<String> ListUserTasks;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +39,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         registration = findViewById(R.id.button_registration);
         registration.setOnClickListener(this);
+
+        ListUserTasks = new ArrayList<>();
+
+        mReference = FirebaseDatabase.getInstance().getReference("attendancetracking-android");
+
     }
 
     @Override
@@ -31,11 +58,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.button_registration :
 
                 if(true) {
+                    mReference.child(user.getUid()).child("Attendance").push().setValue("+").addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Log.d("mLog",task.isSuccessful() + "");
+                            Log.d("mLog",task.getException() + "");
+
+                        }
+                    });
                     Toast.makeText(MainActivity.this,"Вы зарегистрированы на занятии",Toast.LENGTH_SHORT).show();
                     registration.setBackgroundColor(getResources().getColor(R.color.ff));
                     break;
                 }
                 else{
+                    mReference.child(user.getUid()).child("Attendance").push().setValue("X");
                     Toast.makeText(MainActivity.this,"Ошибка сети",Toast.LENGTH_SHORT).show();
                     registration.setBackgroundColor(getResources().getColor(R.color.f2));}
         }
