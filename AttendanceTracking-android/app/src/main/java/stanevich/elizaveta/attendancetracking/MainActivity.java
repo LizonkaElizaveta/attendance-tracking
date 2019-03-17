9732b22b8ea69bb563dcd516a47571ce45dbabfc
Liza <liza_soft_1313@mail.ru>
2019-03-17 22:09:31 +0300

    add button of registration

--- AttendanceTracking-android/app/src/main/java/stanevich/elizaveta/attendancetracking/MainActivity.java
+++ AttendanceTracking-android/app/src/main/java/stanevich/elizaveta/attendancetracking/MainActivity.java
@@ -3,18 +3,34 @@ package stanevich.elizaveta.attendancetracking;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    Button registration;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        registration = findViewById(R.id.button_registration);
+        registration.setOnClickListener(this);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_notification, menu);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.button_registration :
+                Toast.makeText(MainActivity.this,"work",Toast.LENGTH_SHORT).show();
+
+        }
     }
 }
