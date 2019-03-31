fac6d8257e4d8770717864ace98de8b90005ed2f
Liza <liza_soft_1313@mail.ru>
2019-03-31 15:49:16 +0300

    creating DialogUtilities and Constants

---
+++ AttendanceTracking-android/app/src/main/java/stanevich/elizaveta/attendancetracking/constants/AppConstants.java
@@ -0,0 +1,19 @@
+package stanevich.elizaveta.attendancetracking.constants;
+
+public class AppConstants {
+    public static final String BUNDLE_KEY_TITLE = "title";
+    public static final String BUNDLE_KEY_MESSAGE = "message";
+
+    // questions constants
+    public static final String BUNDLE_KEY_YES = "yes";
+    public static final String BUNDLE_KEY_NO = "no";
+    public static final String BUNDLE_KEY_VIEW_ID = "view_id_tex";
+
+    // notification constants
+    public static final String PREF_NOTIFICATION = "pref_notification";
+    public static final String PREF_FONT_SIZE = "pref_font_size";
+    public static final String NEW_NOTI = "new_notification";
+    public static final String BUNDLE_KEY_ITEM = "item";
+    public static final String BUNDLE_KEY_INDEX = "index";
+    public static final String BUNDLE_KEY_DELETE_ALL_NOT = "delete_all_not";
+}
