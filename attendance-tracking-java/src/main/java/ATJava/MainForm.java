268baf05813d9016fc012f3f71e4c52ed434c3a6
Aleksandr Mitiunin <amitiunin@gmail.com>
2019-05-18 19:47:37 +0300

    #31 - реализована работа с БД и отчеты (кроме групп)

--- attendance-tracking-java/src/main/java/ATJava/MainForm.java
+++ attendance-tracking-java/src/main/java/ATJava/MainForm.java
@@ -3,12 +3,14 @@ package ATJava;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.*;
 
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
 import java.awt.event.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -19,10 +21,6 @@ import java.util.Vector;
 
 
 public class MainForm extends JFrame {
-    private JTable allTable;
-    private JTabbedPane reportTable;
-    private JTable presentTable;
-    private JTable absentTable;
     private JButton checkAtt;
     private JPanel contentPane;
     private JPanel centralPane;
@@ -30,6 +28,12 @@ public class MainForm extends JFrame {
     private JPanel dataPane;
     private JPanel changeReportPane;
     private JLabel dbStatus;
+    private JTable studentAttendanceTable;
+    private JComboBox disciplineComboBox;
+    private JTextField groupTextField;
+    private JButton makeReportButton;
+    private JList attendingStudentsList;
+    private JTable classesTable;
 
     private PswDialogResponse AuthData;
     private PswDialogResponse AuthDB;
@@ -65,39 +69,223 @@ public class MainForm extends JFrame {
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
         dataAcessInitialization();
-        DatabaseReference ref = FirebaseDatabase.getInstance()
+        final DatabaseReference ref = FirebaseDatabase.getInstance()
                 .getReference("attendancetracking-android");
         loginFormDatainitializing(ref);
-        while (makeAuth(ref));
+        while (makeAuth(ref)) ;
         userNameUpdate(ref);
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
+        ref.addChildEventListener(new ChildEventListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                DefaultTableModel dtm = new DefaultTableModel();
-                dtm.addColumn("UID");
-                dtm.addColumn("Attendance");
-                for (DataSnapshot userSnapshot: dataSnapshot.getChildren())
-                {
-                    for (DataSnapshot attSnapshot: userSnapshot.child("Attendance").getChildren()
-                         ) {
-                        Vector<String> rowVect = new Vector<String>();
-                        rowVect.add(userSnapshot.getKey());
-                        rowVect.add(attSnapshot.getValue().toString());
-                        dtm.addRow(rowVect);
-                        System.out.print(dtm);
-                    }
-                }
-                allTable.setModel(dtm);
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                if (dataSnapshot.getKey().equals("Disciplines"))
+                    fillDisciplines(dataSnapshot);
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                if (dataSnapshot.getKey().equals("Disciplines"))
+                    fillDisciplines(dataSnapshot);
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getKey().equals("Disciplines"))
+                    fillDisciplines(dataSnapshot);
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                if (dataSnapshot.getKey().equals("Disciplines"))
+                    fillDisciplines(dataSnapshot);
             }
 
             @Override
             public void onCancelled(DatabaseError error) {
             }
         });
+
+        disciplineComboBox.addItemListener(new ItemListener() {
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+                int state = e.getStateChange();
+                if (state == ItemEvent.SELECTED) {
+                    ref.addChildEventListener(new ChildEventListener() {
+                        @Override
+                        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                            fillClassesTable(dataSnapshot);
+                        }
+
+                        @Override
+                        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                            fillClassesTable(dataSnapshot);
+                        }
+
+                        @Override
+                        public void onChildRemoved(DataSnapshot dataSnapshot) {
+                            fillClassesTable(dataSnapshot);
+                        }
+
+                        @Override
+                        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                            fillClassesTable(dataSnapshot);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+                        }
+                    });
+                }
+            }
+        });
+
+        studentAttendanceTable.setDefaultEditor(Object.class, null);
+        attendingStudentsList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        classesTable.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        classesTable.setDefaultEditor(Object.class, null);
+        classesTable.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(final ListSelectionEvent e) {
+                ref.addChildEventListener(new ChildEventListener() {
+                    private void fillIfCorrectSnapshot(DataSnapshot dataSnapshot) {
+                        if (e.getValueIsAdjusting())
+                            return;
+                        for (DataSnapshot classSnapshot : dataSnapshot.getChildren()) {
+                            TableModel model = classesTable.getModel();
+                            int index = classesTable.getSelectedRow();
+
+                            if (classSnapshot.child("discipline").getValue().toString().equals(disciplineComboBox.getSelectedItem().toString()) &&
+                                    classSnapshot.child("timestamp").getValue().toString().equals(model.getValueAt(index, 0))) {
+                                for (DataSnapshot groupSnapshot : classSnapshot.child("groups").getChildren()) {
+                                    if (groupSnapshot.getKey().equals(model.getValueAt(index, 1).toString()))
+                                        fillAttendanceList(groupSnapshot);
+                                }
+                            }
+                        }
+                    }
+
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        fillIfCorrectSnapshot(dataSnapshot);
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                        fillIfCorrectSnapshot(dataSnapshot);
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                        fillIfCorrectSnapshot(dataSnapshot);
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                        fillIfCorrectSnapshot(dataSnapshot);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+
+        attendingStudentsList.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                ref.addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        fillStudentReport(dataSnapshot);
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                        fillStudentReport(dataSnapshot);
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                        fillStudentReport(dataSnapshot);
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                        fillStudentReport(dataSnapshot);
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+            }
+        });
+    }
+
+    private void fillAttendanceList(DataSnapshot dataSnapshot) {
+        DefaultListModel model = new DefaultListModel();
+        for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+            model.addElement(snapshot.getKey());
+        }
+        attendingStudentsList.setModel(model);
+        if(attendingStudentsList.getVisibleRowCount() > 0)
+            attendingStudentsList.setSelectedIndex(0);
+    }
+
+    private void fillClassesTable(DataSnapshot dataSnapshot) {
+        DefaultTableModel dtm = new DefaultTableModel();
+        dtm.addColumn("Date");
+        dtm.addColumn("Group");
+        for (DataSnapshot classSnapshot : dataSnapshot.getChildren()) {
+            if (classSnapshot.child("discipline").getValue().toString().equals(disciplineComboBox.getSelectedItem().toString())) {
+                for (DataSnapshot groupSnapshot : classSnapshot.child("groups").getChildren()) {
+                    Vector<String> row = new Vector<>();
+                    row.add(classSnapshot.child("timestamp").getValue().toString());
+                    row.add(groupSnapshot.getKey());
+                    dtm.addRow(row);
+                }
+            }
+        }
+        classesTable.setModel(dtm);
+        if(classesTable.getRowCount() > 0)
+            classesTable.setRowSelectionInterval(0, 0);
+    }
+
+    private void fillStudentReport(DataSnapshot snapshot) {
+        DefaultTableModel model = new DefaultTableModel();
+        model.addColumn("Date");
+        model.addColumn("Attended");
+        String group = classesTable.getModel().getValueAt(classesTable.getSelectedRow(), 1).toString();
+        String studentName = attendingStudentsList.getModel().getElementAt(attendingStudentsList.getSelectedIndex()).toString();
+        for (DataSnapshot classSnapshot : snapshot.getChildren()) {
+            if (classSnapshot.child("discipline").getValue().toString().equals(disciplineComboBox.getSelectedItem().toString())) {
+                for (DataSnapshot groupSnapshot : classSnapshot.child("groups").getChildren()) {
+                    if (groupSnapshot.getKey().equals(group)) {
+                        Vector<String> row = new Vector<>();
+                        String timestamp = classSnapshot.child("timestamp").getValue().toString();
+                        row.add(timestamp);
+                        if (groupSnapshot.hasChild(studentName)) {
+                            row.add("+");
+                        } else row.add("-");
+                        model.addRow(row);
+                    }
+                }
+            }
+        }
+        studentAttendanceTable.setModel(model);
     }
 
     private void onPress() {
         System.exit(0);
+    }
+
+    private void fillDisciplines(DataSnapshot snapshot) {
+        disciplineComboBox.removeAllItems();
+        for (DataSnapshot disciplineSnapshot : snapshot.getChildren()) {
+            disciplineComboBox.addItem(disciplineSnapshot.getValue().toString());
+        }
     }
 
     public boolean makeAuth(DatabaseReference ref) {
@@ -125,8 +313,9 @@ public class MainForm extends JFrame {
                     .setDatabaseUrl("https://attendancetracking-android.firebaseio.com")
                     .build();
             FirebaseApp.initializeApp(options);
-        }catch (FileNotFoundException fnfExc) {}
-        catch (IOException ioExc) {}
+        } catch (FileNotFoundException fnfExc) {
+        } catch (IOException ioExc) {
+        }
         DatabaseReference ref = FirebaseDatabase.getInstance()
                 .getReference("attendancetracking-android");
         return ref;
@@ -146,7 +335,7 @@ public class MainForm extends JFrame {
         });
     }
 
-    private void userNameUpdate (DatabaseReference rfr) {
+    private void userNameUpdate(DatabaseReference rfr) {
         if (!AuthDB.getEmail().equals(AuthData.getEmail())) {
             Map<String, Object> nameUpdates = new HashMap<>();
             nameUpdates.put("Username", AuthData.getEmail());
